487927d0cef3a65d53dcdb2a222e494e23981b03
Talha Sami <talharajpoot2711@gmail.com>
2025-08-22 11:39:43 +0500

    Time: 254 ms (69.33%) | Memory: 133.6 MB (38.34%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
-        int min_row = -1 , max_row = -1 , min_col = -1 , max_col = -1 ;
-        int n = grid.size() , m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
+        int min_row = n, max_row = -1;
+        int min_col = m, max_col = -1;
 
         for (int i = 0 ; i < n ; i++){
             for (int j = 0 ; j < m ; j++){
-                if (grid[i][j] == 1 && min_row == -1){
-                    min_row = i;
-                    max_row = i;
-                } else if (min_row != -1 && grid[i][j] == 1){
-                    max_row = i;
-                }
-            }
-        }
-
-        for (int i = 0 ; i < m ; i++){
-            for (int j = 0 ; j < n ; j++){
-                if (grid[j][i] == 1 && min_col == -1){
-                    min_col = i;
-                    max_col = i;
-                } else if (min_col != -1 && grid[j][i] == 1){
-                    max_col = i;
+                if(grid[i][j]==1){
+                    min_row=min(i,min_row);
+                    max_row=max(i,max_row);
+                    min_col=min(j,min_col);
+                    max_col=max(j,max_col);
                 }
             }
         }
