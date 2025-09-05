a2d946d8f5bf1448c05519a101fb265415f6e674
Talha Sami <talharajpoot2711@gmail.com>
2025-09-05 05:08:52 +0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (83.78%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -7,13 +7,16 @@ public:
     }
 
     int gcdOfOddEvenSums(int n) {
-        int e  = 0, o = 0;
+        //3ms
+        // int e  = 0, o = 0;
 
-        for (int i = 1 ; i <= n*2 ; i++){
-            if ( i & 1 ) o += i;
-            else e += i;
-        }
+        // for (int i = 1 ; i <= n*2 ; i++){
+        //     if ( i & 1 ) o += i;
+        //     else e += i;
+        // }
 
-        return gcd(e,o);
+        // return gcd(e,o);
+
+        return n;
     }
 };
