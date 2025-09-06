22935dfd156ed03b2a6fd6d5ea81e714f3f207e5
Talha Sami <talharajpoot2711@gmail.com>
2025-09-06 10:31:21 +0500

    Time: 0 ms (100.00%) | Memory: 10.5 MB (37.64%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        // return true;
+
+        sort(piles.begin() , piles.end() , greater<int>());
+        
+        int alice = 0 , bob = 0 , i = 0 , end = piles.size();
+        
+        while (i < end){
+            alice += piles[i++];
+            bob += piles[i++];
+        } 
+
+        return (alice > bob) ? true: false;
+
+        
     }
 };
