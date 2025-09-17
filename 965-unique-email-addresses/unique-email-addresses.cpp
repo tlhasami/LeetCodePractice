dc5b693e22bfa37883c5e8930d06e018865038e7
Talha Sami <talharajpoot2711@gmail.com>
2025-09-17 13:55:33 +0500

    Time: 8 ms (46.85%) | Memory: 18.3 MB (57.59%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_set<string>uniqueEmails;
+
+        for (string email : emails){
+            int idx = 0 , n = email.size();
+            string currentEmail = "";
+
+            for(int i = 0 ; i < n ; i++){
+                char alpha = email[i];
+
+                if (alpha == '@'){
+                    idx++;
+                    currentEmail += "@";
+                } else if (alpha == '.' && idx == 0){
+                    continue;
+                } else if (alpha == '+' && idx == 0){
+                    while (i+1< n && email[i+1] != '@'){
+                        //currentEmail += email[i+1];
+                        i++;
+                    }
+                } else {
+                    currentEmail += alpha;
+                }
+            }
+
+           // cout << currentEmail << endl;
+
+            uniqueEmails.insert(currentEmail);
+
+        }
+
+        return uniqueEmails.size();
+    }
+};
