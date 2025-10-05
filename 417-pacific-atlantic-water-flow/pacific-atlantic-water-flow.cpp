23aea509a23b56ac52b15657fd13612c4b56e2d7
Talha Sami <talharajpoot2711@gmail.com>
2025-10-05 10:15:44 +0500

    Time: 7 ms (62.44%) | Memory: 22.2 MB (82.37%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,50 +1,53 @@
 class Solution {
 public:
-    
-    int dx[4] = {-1, 0, 1, 0};
-    int dy[4] = {0, 1, 0, -1};
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+    int m , n ;
 
-    void dfs(vector<vector<int>>& heights, vector<vector<bool>>& visited, int row, int col) {
-        visited[row][col] = true;
+    void dfs(vector<vector<int>>& heights , vector<vector<bool>>& visited , int x , int y){
+        
+        visited[x][y] = true;
 
-        for (int k = 0 ; k < 4 ; k++) {
-            int nx = row + dx[k];
-            int ny = col + dy[k];
+        for (int i = 0; i < 4; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
 
-            if (nx < 0 || ny < 0 || nx >= heights.size() || ny >= heights[0].size())
+            if (newX < 0 || newX >= m || newY < 0 || newY >= n || visited[newX][newY] ||
+                heights[x][y] > heights[newX][newY])
                 continue;
-
-            if (visited[nx][ny] || heights[nx][ny] < heights[row][col])
-                continue;
-
-            dfs(heights, visited, nx, ny);
+            else 
+                dfs(heights , visited , newX , newY);
         }
     }
 
-    
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        vector<vector<bool>> pac (heights.size(),vector<bool>(heights[0].size(),false));
-        vector<vector<bool>> atl (heights.size(),vector<bool>(heights[0].size(),false));
+        m = heights.size() ;
+        n = heights[0].size();
+        vector<vector<bool>>pacific ( m , vector<bool>(n , false));
+        vector<vector<bool>>atlantic ( m , vector<bool>(n , false));
 
-        for (int i = 0 ; i < heights.size() ; i++){
-            dfs(heights,pac,i,0);
-            dfs(heights,atl,i,heights[0].size() - 1);
+        // rows
+        for(int i = 0 ;i < m ;i++){
+            dfs(heights , pacific , i ,0); // changed rows in first column -> Pacific
+            dfs(heights , atlantic , i , n-1); // changed rows in last column -> Atlantic
         }
 
-        for (int i = 0 ; i < heights[0].size() ; i++){
-            dfs(heights,pac,0,i);
-            dfs(heights,atl,heights.size() - 1,i);
+        for(int i = 0 ; i < n ; i++){
+            dfs(heights , pacific , 0 ,i); // changed cols in first row -> Pacific
+            dfs(heights , atlantic , m-1 , i); // changed cols in last row -> Atlantic
         }
 
-        vector<vector<int>> ans;
-        for (int i = 0 ; i < heights.size(); i++){
-            for (int j = 0 ; j < heights[0].size() ; j++){
-                if (atl[i][j] && pac[i][j]) 
-                    ans.push_back({i,j});
+
+        vector<vector<int>>result;
+
+        for(int i = 0 ; i< m ;i++){
+            for (int j = 0 ; j < n ; j++){
+                if (pacific[i][j] && atlantic[i][j]) 
+                    result.push_back({i,j});
             }
         }
 
-        return ans;
+        return result;
 
     }
 };
