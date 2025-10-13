354d0c2aa644c42b123d8adea1baaa54fd642d82
Talha Sami <talharajpoot2711@gmail.com>
2025-10-13 18:41:53 +0500

    Time: 0 ms (100.00%) | Memory: 31.2 MB (73.35%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -12,9 +12,8 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if (!root) return 0;
+       if (!root) return 0;
 
-        return countNodes(root->left) +  countNodes(root->right)  + 1;
-        
+       return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
