c9d894dac32e48d505f31863a81b0927ad9b05e8
Talha Sami <talharajpoot2711@gmail.com>
2025-10-15 19:35:01 +0500

    Time: 0 ms (100.00%) | Memory: 21.5 MB (34.57%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -14,13 +14,18 @@ public:
 
     bool helper(TreeNode* root, int targetSum , int current = 0) {
         if (!root) return false;
-        if (current+root->val == targetSum && !root->left && !root->right) return true;
 
-        return helper(root->left,targetSum,current + root->val) || 
-               helper(root->right,targetSum,current + root->val) ;
+        current += root->val;
+
+        if (!root->left && !root->right)
+            return current == targetSum;
+        
+
+        return helper(root->left,targetSum,current ) || helper(root->right,targetSum,current) ;
     }
 
     bool hasPathSum(TreeNode* root, int targetSum) {
-        return helper(root,targetSum,0);
+        if (!root) return false;
+        return helper(root,targetSum) ;
     }
 };
