229ae18e89109f3899db5f95a2a008420a7888ea
Talha Sami <talharajpoot2711@gmail.com>
2025-10-15 20:01:59 +0500

    Time: 5 ms (1.08%) | Memory: 26.1 MB (8.09%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if (!root) return nullptr;
+
+        if (depth == 1){
+            TreeNode* tree = new TreeNode(val);
+            tree->left = root;
+            root = tree;
+            return root;
+        }
+
+        depth --;
+        queue<TreeNode*>q;
+        q.push(root); 
+
+        while (!q.empty()){
+            int size = q.size(); 
+            cout << depth << " ";
+            for (int i = 0 ; i < size ; i++){
+                TreeNode* tree = q.front(); 
+                q.pop();
+
+                if (depth == 1){
+                    TreeNode* toAddLeft = new TreeNode(val);
+                    toAddLeft->left = tree->left;
+                    TreeNode* toAddRight = new TreeNode(val);
+                    toAddRight->right = tree->right;
+
+                    tree->left = toAddLeft;
+                    tree->right = toAddRight;
+                }
+
+
+                if (tree->left) q.push(tree->left);
+                if (tree->right) q.push(tree->right);
+            }
+
+            if (depth == 1) return root;
+
+            depth --;
+        }
+
+        return root;
+
+    }
+};
