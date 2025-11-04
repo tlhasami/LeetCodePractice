6934a965462bae693ef093786089fe66aa2e1a59
Talha Sami <talharajpoot2711@gmail.com>
2025-11-04 19:08:27 +0500

    Time: 25 ms (72.04%) | Memory: 16.2 MB (89.53%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
-    int dx[4] = {-1, 1, 0, 0}; // up, down, left, right
-    int dy[4] = {0, 0, -1, 1};
-    vector<vector<bool>> visited;
-
-    void bfs(vector<vector<char>>& grid, int row, int col) {
-        visited[row][col] = true;
-        queue<pair<int,int>> q;
-        q.push({row,col});
-
-        int n = grid.size(), m = grid[0].size();
-
-        while(!q.empty()){
-            auto [x,y] = q.front();
-            q.pop();
-
-            for (int k = 0 ;k < 4 ;k++){
-                int newPos_x = x + dx[k];
-                int newPos_y = y + dy[k];
-                if (newPos_x >= 0 && newPos_y >= 0 && newPos_x < n && newPos_y < m && !visited[newPos_x][newPos_y] && grid[newPos_x][newPos_y]  == '1'){
-                    visited[newPos_x][newPos_y] = true;
-                    q.push({newPos_x, newPos_y});
-                }
-            }
-        }
-    }
-
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        int count = 0;
-
-        visited = vector<vector<bool>>(n, vector<bool>(m, false));
+    void dfs(vector<vector<char>>& grid , int row , int col , const int& n , const int& m){
+        if (row >= n || col >= m || row < 0 || col < 0 || grid[row][col]=='0')
+            return; 
         
+        
+        
+        grid[row][col] = '0';
+
+        dfs(grid,row -1, col , n , m);
+        dfs(grid,row +1, col , n , m);
+        dfs(grid,row , col+1 , n , m);
+        dfs(grid,row , col-1, n , m);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size() , m =grid[0].size() , islands = 0;
+
         for (int i = 0 ; i < n ; i++){
             for (int j = 0 ; j < m ; j++){
-                if (!visited[i][j] && grid[i][j] == '1'){
-                    bfs(grid,i,j);
-                    count++;
+                if (grid[i][j] == '1'){
+                    ++islands;
+                    dfs(grid,i,j,n,m);
                 }
             }
         }
 
-        return count; 
+        return islands;
+
     }
 };
