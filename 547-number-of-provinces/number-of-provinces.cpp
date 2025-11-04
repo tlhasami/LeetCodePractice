85fea13959d2ca88ce48465d0ec48a9f8b46983f
Talha Sami <talharajpoot2711@gmail.com>
2025-11-04 07:46:13 +0500

    Time: 75 ms (5.15%) | Memory: 20.7 MB (33.71%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int iterateProvince(vector<bool>&visited , vector<vector<int>>&list , const int& n , int i = 0){
+
+        if (n == i) return 0;
+
+        queue<int>qu;
+        qu.push(i);
+        visited[i] = true;
+        while (!qu.empty()){
+            int level = qu.size();
+
+            for (int i = 0 ; i < level ; i++){
+                int temp = qu.front() ; qu.pop();
+
+                for (int& val : list[temp])
+                    if (!visited[val]) {
+                        visited[val] = true;
+                        qu.push(val);
+                    }
+                
+            }
+        }
+
+        return 1;
+
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+
+        vector<vector<int>>list(n);
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j < n ; j++){
+                //cout << isConnected[i][j] << " ";
+                if (isConnected[i][j] == 1 && i!=j){
+                    list[i].push_back(j);
+                }
+            }
+        }
+
+        for (auto& l :list){
+            for (auto& a : l){
+                cout << a << " ";
+            } cout << endl;
+        }
+
+
+
+        vector<bool>visited(n,false);
+        int count = 0;
+        for (int i = 0 ; i < n ; i++){
+
+            if (!visited[i])
+                count+=iterateProvince(visited,list,n ,i);
+
+            // for (bool a : visited) cout << a << " ";
+            // cout << endl;
+        }
+        return count;
+    }
+};
