db0e8f3f6fac78998307edb50fb3225a38bbf15f
Talha Sami <talharajpoot2711@gmail.com>
2025-11-17 09:10:03 +0500

    Time: 73 ms (5.00%) | Memory: 20.6 MB (35.99%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -59,3 +59,40 @@ public:
         return count;
     }
 };
+
+/*
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+        visited[node] = true;
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor])
+                dfs(neighbor, adj, visited);
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>> adj(n);
+
+        // Build adjacency list
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j)
+                    adj[i].push_back(j);
+            }
+        }
+
+        vector<bool> visited(n, false);
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited);
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+};
+*/
