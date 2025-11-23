abefbcc809203631ed7404e42c618a46e26f6597
Talha Sami <talharajpoot2711@gmail.com>
2025-11-23 23:32:38 +0500

    Time: 19 ms (16.78%) | Memory: 19.3 MB (23.40%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    void dfs(vector<vector<int>>& graph , vector<vector<int>>& result , vector<int>path ,vector<bool>&visited, const int& n , int idx = 0 ){
+
+        visited[idx] = true;
+        path.push_back(idx);
+
+        if (idx == n){
+            result.push_back(path);
+        }
+
+        for (int& edge : graph[idx] )
+            if (!visited[edge]) 
+                dfs(graph ,result,path,visited,n,edge);
+            
+        visited[idx] = false;
+    }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        
+        vector<vector<int>>result;
+        vector<bool>visited( graph.size() , false);
+        dfs(graph,result,{} , visited,graph.size()-1 , 0);
+
+        return result;
+        
+    }
+};
