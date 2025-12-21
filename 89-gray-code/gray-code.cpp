c8325e7ffc7a5a6212e451e2d217f6be186120a8
Talha Sami <talharajpoot2711@gmail.com>
2025-12-21 02:59:34 -0800

    Time: 0 ms (100.00%) | Memory: 14 MB (89.69%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -60,7 +60,8 @@ public:
     vector<int> grayCode(int n) {
         int total = 1 << n;
         vector<int> res(total);
-        for (int i = 0; i < total; i++) res[i] = (i ^ (i >> 1));
+        for (int i = 0; i < total; i++) 
+            res[i] = (i ^ (i >> 1));
         return res;
     }
 };
