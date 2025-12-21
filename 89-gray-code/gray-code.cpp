7bb3cee1c7683265327f07d68e0045ab8f1eb549
Talha Sami <talharajpoot2711@gmail.com>
2025-12-21 02:56:08 -0800

    Time: 51 ms (5.26%) | Memory: 23 MB (10.72%) - LeetSync

---
+++ 89-gray-code/gray-code.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string numberToBinary(int n){
+        string binary = "";
+        while (n > 0){
+            binary.append( n&1 ? "1": "0");
+            n >>= 1;
+        }
+
+        reverse(binary.begin(), binary.end());
+
+        return binary;
+    }
+
+    int binaryToNumber(string bin){
+        int val = 0 , n = bin.length() , idx = 0;
+        for (int i = n-1; i >=0 ; i--){
+            if (bin[i] == '1')
+                val += 1<<idx;
+
+            ++idx;
+        }
+
+        return val;
+    }
+
+    int getGreyCode(int n){
+        string bin = numberToBinary(n);
+
+        if (bin.length() < 1) return bin.length();
+        int size = bin.length() ;
+        string grey(size , '0');
+
+        grey[0] = bin[0];
+
+        for (int i = 1 ; i < size ; i++){
+            if ((bin[i]-'0')^(bin[i-1]-'0'))
+                grey[i] = '1';
+            else
+                grey[i] = '0';
+        }
+        
+        return binaryToNumber(grey);
+    }
+
+    vector<int> grayCode(int n) {
+        vector<int>result;
+        int maxRange = 1<<(n) ;
+        --maxRange;
+        //cout << "GAME : " << maxRange << endl;
+
+        for (int i = 0 ; i <= maxRange ; i++){
+            //cout << i << " " << getGreyCode(i) << endl;
+            result.push_back(getGreyCode(i));
+        }
+
+        return result;
+    }
+};
