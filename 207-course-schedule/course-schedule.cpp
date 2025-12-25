8eb78d2f76d4f3bd9ac341ef928566a89a256bd2
Talha Sami <talharajpoot2711@gmail.com>
2025-12-25 07:08:00 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.81%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        vector<int>indegree(numCourses,0);
+        vector<int>topo;
+        vector<vector<int>>graph(numCourses);
+
+        for (auto& pre : prerequisites){
+            graph[pre[1]].push_back(pre[0]);
+        }
+
+        for (auto& adjencylist : graph){
+            for (int& node :adjencylist){
+                indegree[node]++;
+            }
+        }
+
+        queue<int>qu;
+        for (int i = 0 ; i < numCourses ; i++)
+            if (indegree[i] == 0)
+                qu.push(i);
+        
+        while(!qu.empty()){
+            int temp = qu.front();
+            qu.pop();
+
+            topo.push_back(temp);
+
+            for (auto& node : graph[temp] ){
+                --indegree[node];
+                if (indegree[node] == 0)
+                    qu.push(node);
+            }
+        }
+
+        if (topo.size() == numCourses)
+            return true;
+        
+        return false;
+    }
+};
