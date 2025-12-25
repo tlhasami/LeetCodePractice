166577794eab60ae0b3bb2db2f7fb16858b065e9
Talha Sami <talharajpoot2711@gmail.com>
2025-12-25 09:30:17 -0800

    Time: 15 ms (72.21%) | Memory: 34.4 MB (95.47%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, 1, 0, -1};
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size() , m = mat[0].size();
+
+        queue<pair<int,int>>qu;
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j < m ; j++){
+                if (mat[i][j] == 0){
+                    qu.push({i,j});
+                } else{
+                    mat[i][j] = INT_MAX;
+                }
+            }
+        }
+
+        while (!qu.empty()){
+            auto temp = qu.front();
+            qu.pop();
+
+            for (int i = 0 ; i < 4 ; i++){
+                int row = temp.first + dx[i];
+                int col = temp.second + dy[i];
+
+                if (row < 0 || col < 0 || row >= n || col >= m)
+                    continue;
+
+                
+                if (mat[row][col] > mat[temp.first][temp.second]+1){
+                    mat[row][col] = mat[temp.first][temp.second]+1;
+                    qu.push({row,col});
+                }
+                    
+                
+            }
+            
+
+        }    
+
+        return mat;
+    }
+};
