ce9606ef7b497a34bfbd1843a35056904b47ba81
Talha Sami <talharajpoot2711@gmail.com>
2025-12-25 10:45:10 -0800

    Time: 7 ms (76.82%) | Memory: 33 MB (63.23%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -4,37 +4,40 @@ public:
     int dx[4] = {-1, 0, 1, 0};
     int dy[4] = {0, 1, 0, -1};
 
-    void dfs(vector<vector<int>>& grid , int row, int col){
-        if (row  < 0 || col < 0 || row >= grid.size() || col >= grid[0].size() || grid[row][col] != 1 )
-            return ;
+    void dfs(vector<vector<int>>& board, int r, int c, int n, int m) {
+        board[r][c] = 0;
 
-        grid[row][col] = -1;
+        for (int i = 0; i < 4; i++) {
+            int row = r + dx[i];
+            int col = c + dy[i];
 
-        for (int k = 0 ; k < 4 ; k++){
-            dfs(grid,row + dx[k] , col + dy[k]);
+            if (row < 0 || col < 0 || row >= n || col >= m || board[row][col] != 1)
+                continue;
+
+            dfs(board, row, col, n, m);
         }
     }
 
-    int numEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size() , m = grid[0].size();
+    int numEnclaves(vector<vector<int>>& board) {
+        int n = board.size() , m = board[0].size();
 
-        for (int i = 0 ; i < n ; i++){
-            dfs(grid,i,0);
-            dfs(grid,i,m-1);
-        }
-        for (int i = 0 ; i < m ; i++){
-            dfs(grid,0,i);
-            dfs(grid,n-1,i);
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 1)     dfs(board, i, 0, n, m);
+            if (board[i][m-1] == 1)   dfs(board, i, m-1, n, m);
         }
 
-        int count = 0;
+        for (int i = 0; i < m; i++) {
+            if (board[0][i] == 1)     dfs(board, 0, i, n, m);
+            if (board[n-1][i] == 1)   dfs(board, n-1, i, n, m);
+        }
 
-        for (int i = 0 ; i < n ; i++){
-            for (int j = 0 ; j < m ; j++){
-                count += grid[i][j] == 1 ? 1 : 0 ; 
+        int landCells = 0;
+        for (auto& line : board){
+            for (auto& value : line){
+                landCells += value;
             }
         }
 
-        return count;
+        return landCells;
     }
 };
